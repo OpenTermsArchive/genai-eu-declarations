1d6e1e3dafc400948bc48a3282faa96af6f7d2bb
Zenobio de Almeida <zenobioarn@gmail.com>
2026-01-17 17:37:17 +0100

    Add Claude.ai filter

--- declarations/Claude.ai.filters.js
+++ declarations/Claude.ai.filters.js
@@ -1,5 +1,6 @@
 export function removeRedirectFromLinks(document) {
   const links = document.querySelectorAll('a');
+
   links.forEach(link => {
     link.href = link.href.replace(/\/redirect\/[^/]+\//, '/');
   });
